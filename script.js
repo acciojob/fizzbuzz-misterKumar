1a0cfd6f867fd5ed2c11d5646bb1d6b9ce8a5740
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-25 18:04:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let str="";
+for(let i=0;i<=20;i++){
+	
+		if(i%5==0&&i%3==0){
+		str+="FizzBuzz \n";
+	}
+else	if (i%5==0) {
+		str+="Buzz \n";
+	}
+else	if (i%3==0) {
+		str+="Fizz \n";
+	}
+else {
+	str+=i+"\n";
+}
+
+}
+alert(str);
